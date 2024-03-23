60264f91c3be0bfd205f3b3eb2d3f07b0d642f14
TolisSth <tolishalis@gmail.com>
2024-03-23 19:44:24 +0200

    Added refreshing mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,53 @@
-// Author: Apostolos Chalis 2024
-// Rust CLI system profiler
-use sysinfo::{
-    Disks, Networks, System,
-};
+use sysinfo::{Disks, Networks, System};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     println!("System profiler Version 0.0.1\nApostolos Chalis 2024\n");
 
     let mut sys = System::new_all();
     sys.refresh_all();
-    
-    // RAM usage
-    println!("==> RAM usage.");
-    println!("Used memory : {} bytes / {} bytes", sys.used_memory(), sys.total_memory());
-    print!("\n");
 
-    // CPU usage
-    println!("==> CPU usage.");
-    sys.refresh_cpu();
-    let mut counter = 0;
+    loop {
+        // Clear the terminal
+        print!("\x1B[2J\x1B[1;1H");
 
-    for cpu in sys.cpus() {
-        counter+= 1;
-        print!("CPU {}: {}% ",counter, cpu.cpu_usage());
-    }
-    print!("\n");
-  
-    // Network usage
-    println!("\n==> Network usage.");
-    let networks = Networks::new_with_refreshed_list();
+        // RAM usage
+        println!("==> RAM usage.");
+        println!("Used memory : {} bytes / {} bytes", sys.used_memory(), sys.total_memory());
+        print!("\n");
 
-    for (interface_name, data) in &networks {
-        println!(
-        "{interface_name}: {} B (down) / {} B (up)",
-        data.total_received(),
-        data.total_transmitted(),
-        );
-    }
-    print!("\n");
-    
-    // Disks usage
-    println!("==> Disks usage.");
-    let disks = Disks::new_with_refreshed_list();
-    for disk in &disks {
-        println!("{disk:?}");
+        // CPU usage
+        println!("==> CPU usage.");
+        sys.refresh_cpu();
+        let mut counter = 0;
+        for cpu in sys.cpus() {
+            counter += 1;
+            print!("CPU {}: {}% ", counter, cpu.cpu_usage());
+        }
+        print!("\n");
+
+        // Network usage
+        println!("\n==> Network usage.");
+        let networks = Networks::new_with_refreshed_list();
+        for (interface_name, data) in &networks {
+            println!(
+                "{interface_name}: {} B (down) / {} B (up)",
+                data.total_received(),
+                data.total_transmitted(),
+            );
+        }
+        print!("\n");
+
+        // Disks usage
+        println!("==> Disks usage.");
+        let disks = Disks::new_with_refreshed_list();
+        for disk in &disks {
+            println!("{disk:?}");
+        }
+
+        // Sleep for a while before refreshing
+        thread::sleep(Duration::from_secs(1)); // Adjust refresh rate as needed
     }
 }
+
