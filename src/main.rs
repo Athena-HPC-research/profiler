7154bd089005b1581749e7e6a722c0137223a975
TolisSth <tolishalis@gmail.com>
2024-03-25 13:46:42 +0200

    Added calculate_cpu_total_usage()

--- src/main.rs
+++ src/main.rs
@@ -36,15 +36,23 @@ fn main() {
         println!("==> CPU usage.");
         sys.refresh_cpu();
         let mut counter = 0;
+        let mut cpu_usages: Vec<f64> = Vec::new();
+
         for cpu in sys.cpus() {
             counter += 1;
             print!("CPU {}: {}% ", counter, cpu.cpu_usage());
+            cpu_usages.push(cpu.cpu_usage() as f64); 
         }
+        print!("\n");
+
+        let size_of_vector = cpu_usages.len() as u16; 
+        println!("CPU utilization: {}", calculate_cpu_total_usage(cpu_usages, size_of_vector)); 
         print!("\n");
 
         // Network usage
         println!("\n==> Network usage.");
         let networks = Networks::new_with_refreshed_list();
+
         for (interface_name, data) in &networks {
             println!(
                 "{interface_name}: {} B (down) / {} B (up)",
@@ -58,6 +66,7 @@ fn main() {
         // Disks usage
         println!("==> Disks usage.");
         let disks = Disks::new_with_refreshed_list();
+
         for disk in &disks {
             println!("{disk:?}");
         }
@@ -78,7 +87,20 @@ fn calculate_used_ram(u_ram:u64, t_ram:u64) -> u16{
     
     let div: f64 = u_ram as f64 / t_ram as f64;
     let percentage = (div * 100.0) as u16;
+
     percentage
+}
+
+fn calculate_cpu_total_usage(core_usages:Vec<f64>, cores:u16) -> f64{
+    // u_cpu = Utilized CPU 
+    // cores = Number of cores in the socket
+    
+    let f_cores = cores as f64;
+
+    let total_core_usage:f64 = core_usages.iter().sum();
+    let utilization = total_core_usage / f_cores;
+
+    utilization
 }
 
 fn calculate_pps(up_net:u64, down_net:u64) -> u64{
